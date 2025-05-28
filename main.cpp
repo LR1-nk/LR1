5da874d9318a38955e2f65f51af83ebf79c7a298
Nikita <nikita_kap_2000@mail.ru>
2025-05-28 20:50:20 +0300

    Седьмой коммент

--- main.cpp
+++ main.cpp
@@ -6,6 +6,6 @@ int main() {
     cout << "Enter two integers: ";
     cin >> a >> b;
     cout << "Sum: " << a + b << endl;
-    cout << "Difference: " << a - b << endl;
+    cout << "Product: " << a * b << endl;
     return 0;
 }
